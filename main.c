b4479a4a5fe71a754fb9162fb9e0c01454874abe
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-03-14 23:02:20 +0100

    addding basic main

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include "matrix.h"
+
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
+    printf("Missing filename\n");
+  }
+  FILE *file = fopen(argv[1], "r");
+  if (file == NULL){
+    perror("Error");
+    exit(-1);
+  }
+  matrix m = parse_file_input(file);
+  fclose(file);
+  return 0;
+}
