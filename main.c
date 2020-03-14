2393ae7d556e29f523b290356ee17b97e03db8e3
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-03-15 00:42:56 +0100

    Improved error handling in main.c

--- main.c
+++ main.c
@@ -7,8 +7,10 @@ int main(int argc, char **argv)
 {
   if (argc < 2)
   {
-    printf("Missing filename\n");
-  }
+    printf("Missing filenames\n");
+  } else if( argc < 3){
+    printf("Missing 2nd filename");
+  } else {
   FILE *file = fopen(argv[1], "r");
   if (file == NULL){
     perror("Error");
@@ -18,4 +20,6 @@ int main(int argc, char **argv)
   fclose(file);
   destroy_matrix(m);
   return 0;
+  }
+  return -1;
 }
