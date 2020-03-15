8c2b6d4081d3534adeb7f147fccacd62ad9ad693
1stDimension <23144389+1stDimension@users.noreply.github.com>
2020-03-15 16:54:49 +0100

    m

--- main.c
+++ main.c
@@ -5,34 +5,41 @@
 
 int main(int argc, char **argv)
 {
-  if (argc < 2)
+  if (argc < 2) {
+    printf("Missing thread count\n");
+  } else if(argc < 3)
   {
     printf("Missing filenames\n");
-  } else if( argc < 3){
+  } else if( argc < 4){
     printf("Missing 2nd filename");
   } else {
-  FILE *file = fopen(argv[1], "r");
-  if (file == NULL){
-    perror("Error");
-    exit(-1);
-  }
-  matrix first = parse_file_input(file);
-  fclose(file);
+    FILE *file = fopen(argv[2], "r");
+    if (file == NULL){
+      perror("Error");
+      exit(-1);
+    }
+    matrix first = parse_file_input(file);
+    fclose(file);
 
-  file = fopen(argv[1], "r");
-  if (file == NULL){
-    perror("Error");
-    exit(-1);
-  }
-  matrix second = parse_file_input(file);
-  fclose(file);
+    file = fopen(argv[3], "r");
+    if (file == NULL){
+      perror("Error");
+      exit(-1);
+    }
+    matrix second = parse_file_input(file);
+    fclose(file);
+    matrix result = create_matrix(first.row_count, second.column_count);
+    for(int i = 0; i < first.row_count; i++){
+      for(int j = 0; j < second.column_count; j++){
+        double entry = matrix_row_column(first, i, second, j);
+        set(result, i, j, entry);
+      }
+    }
 
-  double result = matrix_row_column(first, 0, second, 0);
-  printf("answer = %lf\n", result);
 
-  destroy_matrix(first);
-  destroy_matrix(second);
-  return 0;
-  }
+    destroy_matrix(first);
+    destroy_matrix(second);
+    return 0;
+    }
   return -1;
 }
